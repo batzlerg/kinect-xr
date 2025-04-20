47443743a6ed041c22bfacef8e3eaeb27229351f
graham <batzler@pm.me>
2025-04-20 09:42:28 -0700

    superb basic libfreenect usage

--- src/kinect_test.cpp
+++ src/kinect_test.cpp
@@ -1,6 +1,21 @@
+#include <libfreenect/libfreenect.h>
+
 #include <iostream>
 
 int main() {
-  std::cout << "Hello, Kinect XR!" << std::endl;
+  std::cout << "Kinect XR test program" << std::endl;
+
+  freenect_context* ctx;
+  if (freenect_init(&ctx, nullptr) < 0) {
+    std::cerr << "Failed to initialize freenect context" << std::endl;
+    return 1;
+  }
+
+  // get number of devices
+  int num_devices = freenect_num_devices(ctx);
+  std::cout << "Found " << num_devices << " Kinect device(s)" << std::endl;
+
+  freenect_shutdown(ctx);
+
   return 0;
 }
